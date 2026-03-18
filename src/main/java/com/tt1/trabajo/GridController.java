b5e31a0a4a0dad9c38991f142c6224fe63a57254
alpiedra <alpiedra@unirioja.es>
2026-03-18 10:53:44 +0100

    fin p5

--- src/main/java/com/tt1/trabajo/GridController.java
+++ src/main/java/com/tt1/trabajo/GridController.java
@@ -24,24 +24,24 @@ import modelo.Punto;
 
 @Controller
 public class GridController {
-	private final InterfazContactoSim ics;
-	private final Logger logger;
-	
-	public GridController(InterfazContactoSim ics, Logger logger) {
-		this.ics = ics;
-		this.logger = logger;
-	}
-	
-	@GetMapping("/grid")
+    private final InterfazContactoSim ics;
+    private final Logger logger;
+
+    public GridController(InterfazContactoSim ics, Logger logger) {
+        this.ics = ics;
+        this.logger = logger;
+    }
+
+    @GetMapping("/grid")
     public String solicitud(@RequestParam int tok, Model model) {
-		DatosSimulation ds = ics.descargarDatos(tok);
+        DatosSimulation ds = ics.descargarDatos(tok);
         model.addAttribute("count", ds.getAnchoTablero());
         model.addAttribute("maxTime", ds.getMaxSegundos());
         Map<String, String> colors = new HashMap<>();
         for(var t = 0; t < ds.getMaxSegundos(); t++) {
-        	for(Punto p : ds.getPuntos().get(t)) {
-        		colors.put(t+"-"+p.getY()+"-"+p.getX(), p.getColor());
-        	}
+            for(Punto p : ds.getPuntos().get(t)) {
+                colors.put(t+"-"+p.getY()+"-"+p.getX(), p.getColor());
+            }
         }
         model.addAttribute("colors", colors);
         return "grid";
